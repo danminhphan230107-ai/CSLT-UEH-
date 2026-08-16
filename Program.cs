2b9b918016973d27faadde8e1eba24098a1849d5
Phan Minh Đan <dannn.01007@gmail.com>
2026-08-16 17:11:55 +0700

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace CSLT
+{
+    class Buoi1
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Đây là bài tập buổi 1");
+        }
+    }
+}
+
