9cdfb581382a2919aae642152ea77042ecc7f917
danminhphan230107-ai <dannn.01007@gmail.com>
2026-08-17 14:52:39 +0700

    buoi2

---
+++ Buoi2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Transactions;
+namespace Buoi2
+{
+    class Buoi2
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your name ");
+            string HoVaTen = Console.ReadLine();
+            Console.Write("Enter your age ");
+            int age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"{HoVaTen} - {age}");
+            Console.ReadKey();
+        }
+    }
+}
