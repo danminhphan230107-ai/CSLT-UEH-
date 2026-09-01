4524d5fbcf818faea27e5731822a5f34cfef64c2
Phan Minh Đan <dannn.01007@gmail.com>
2026-09-02 00:03:39 +0700

    up bai len

--- BT_Ex3/Program.cs
+++ BT_Ex3/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Runtime.ConstrainedExecution;
+using System.Runtime.Intrinsics.X86;
 namespace CSLT
 {
     class BT_Ex3
@@ -7,13 +8,15 @@ namespace CSLT
         static void Main(string[] args)
         {
             Console.OutputEncoding=System.Text.Encoding.UTF8;
-            Console.WriteLine($"Nhập thứ bài muốn chạy \nNhập \"0\" để chọn chạy tất cả");
+            Console.WriteLine($"Nhập thứ tự bài muốn chạy \nNhập \"0\" để chọn chạy tất cả");
             string chon = Console.ReadLine();
             switch (chon)
             {
-                case "0": Bai_1(); break;
+                case "0": Bai_1(); Bai_2(); Bai_3(); break;
                 case "1": Bai_1(); break;
                 case "2": Bai_2(); break;
+                case "3": Bai_3(); break;
+                case "4": Bai_4(); break;
             }
         }
 
@@ -44,7 +47,65 @@ namespace CSLT
                 }
             }
             Console.WriteLine($"Số lớn nhất trong các số đã nhập là {soMax}");
+        }
 
+        static void Bai_3()
+        {
+            Console.WriteLine("Nhập độ dài ba cạnh a, b, c của tam giác lần lượt là");
+            double[] canh = Array.ConvertAll(Console.ReadLine().Split(new char[] {' ',','}, StringSplitOptions.RemoveEmptyEntries), double.Parse);
+            double a = canh[0];
+            double b = canh[1];
+            double c = canh[2];
+
+            if (a+b > c || a+c > b || b+c > a)
+            {
+                if (a==b && b==c && a==c)
+                {
+                    Console.WriteLine($"Ba canh a = {a}, b = {b}, c = {c} tạo thành tam giác đều");
+                }
+                else if (a==b || b==c || a==c)
+                {
+                    Console.WriteLine($"Ba canh a = {a}, b = {b}, c = {c} tạo thành tam giác cân");
+                }
+                else
+                {
+                    Console.Write($"Ba canh a = {a}, b = {b}, c = {c} tạo thành tam giác");
+                }
+            }
+            else
+            {
+                Console.WriteLine($"Ba canh a = {a}, b = {b}, c = {c} không tạo thành tam giác");
+            }
+        }
+
+        static void Bai_4()
+        {
+            Console.WriteLine($"Nhập toạ độ (x,y) lần lượt là");
+            int[] toaDo = Array.ConvertAll(Console.ReadLine().Split(new char[] {' ', ','}, StringSplitOptions.RemoveEmptyEntries), int.Parse);
+            int x = toaDo[0];
+            int y = toaDo[1];
+            if (x>0)
+            {
+                if (y>0)
+                {
+                    Console.WriteLine("First Quadrant");
+                }
+                else if (y<0)
+                {
+                    Console.WriteLine("Forth Quadrant");
+                }
+            }
+            else if (x<0)
+            {
+                if (y>0)
+                {
+                    Console.WriteLine("Second Quadrant");
+                }
+                else if (y<0)
+                {
+                    Console.WriteLine("Third Quadrant");
+                }
+            }
         }
     }
 }
